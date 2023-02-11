d42b75569c89d4031cc3b1c8c2cc10bf457b6321
vis <48868398+vis2x@users.noreply.github.com>
2023-02-11 18:15:02 +0000

    Map array of random numbers to Die components

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 import Die from "./components/Die"
 
 export default function App() {
+
+    const [dice, setDice] = React.useState(allNewDice)
+
+    function allNewDice(){
+        let newDice = [];
+        for (let i=0;i<10;i++){
+            newDice.push(Math.floor(Math.random() * 6) + 1);
+        }
+        return newDice;
+    }
+
+    const diceElements = dice.map(num => <Die num={num} />)
+
     return (
         <main>
             <div className="dice--container">
-                <Die num="1" />
-                <Die num="1" />
-                <Die num="1" />
-                <Die num="1" />
-                <Die num="1" />
-
-                <Die num="1" />
-                <Die num="1" />
-                <Die num="1" />
-                <Die num="1" />
-                <Die num="1" />
-
+                {diceElements}
             </div>
         </main>
     )
