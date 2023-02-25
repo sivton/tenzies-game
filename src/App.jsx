11e86644bc908741e4d37afaa68258c8d60eb593
vis <48868398+vis2x@users.noreply.github.com>
2023-02-25 14:43:27 +0000

    Added new game creation after winning

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
         const allSameValue = dice.every(die => die.value===firstValue)
         if (allHeld && allSameValue) {
             setTenzies(true)
-            console.log("You won!")
+            // console.log("You won!")
         }
     }, [dice])
 
@@ -43,9 +43,14 @@ export default function App() {
 
 
     function rollDice() {
-        setDice(oldDice => oldDice.map(die => {
-            return die.isHeld ? die : generateNewDie()
-        }))
+        if (!tenzies) {
+            setDice(oldDice => oldDice.map(die => {
+                return die.isHeld ? die : generateNewDie()
+            }))
+        } else {
+            setTenzies(false)
+            setDice(allNewDice)
+        }
     }
 
     function holdDice(id){
